635256400d0d9140cc07b85afdf8d5e897790405
Babbur Vyshnavi <vyshugoud11@gmail.com>
2021-04-09 21:43:02 +0530

    ADD: prints max value

--- src/main/java/com/generics/GenericType.java
+++ src/main/java/com/generics/GenericType.java
@@ -12,12 +12,15 @@ public class GenericType <E extends Comparable> {
     public static <E extends Comparable> E max(E a, E b, E c) {
         if(a.compareTo(b) > 0) {
             if(a.compareTo(c) > 0) {
+                System.out.println("Maximum of " + a + ", " + b + ", " + c + " : " + a);
                 return a;
             }
         }
         if(b.compareTo(c) > 0) {
+            System.out.println("Maximum of " + a + ", " + b + ", " + c + " : " + b);
             return b;
         }
+        System.out.println("Maximum of " + a + ", " + b + ", " + c + " : " + c);
         return c;
     }
 }
